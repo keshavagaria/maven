dc71def56153a45ca13a6c618cd62067c6f9148c
vishu <vishal267s@gmail.com>
2020-05-27 02:28:17 +0530

    maven simple project

---
+++ Demo/src/main/java/com/company/Demo/App.java
@@ -0,0 +1,13 @@
+package com.company.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
